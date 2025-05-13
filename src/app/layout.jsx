9d41301d1cef5aa0ba8d26676788c1dc46508e94
Hajran <hajranzahid19@gmail.com>
2025-05-12 20:49:19 -0400

    modify homepage

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Menubar from "@/Components/menubar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Menubar></Menubar>
         {children}
       </body>
     </html>
